f209170d81d7a21649ec4b177f706cea07ce40bb
m0mchil <pishtov@gmail.com>
2010-12-07 15:34:48 +0200

    using brocktice's BitcoinMiner, added time update every second, removed maxbase (obsolete by time update)

--- BitcoinMiner.py
+++ BitcoinMiner.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 import sys
 import numpy as np
 import pyopencl as cl
@@ -12,133 +10,142 @@ from jsonrpc.proxy import JSONRPCException
 
 
 def uint32(x):
-        return x & 0xffffffffL
+	return x & 0xffffffffL
 
 def rot(x, y):
-        return (x<<y | x>>(32-y))
-        
+	return (x<<y | x>>(32-y))
+	
 def sharound(a,b,c,d,e,f,g,h,x,K):
-        t1=h+(rot(e, 26)^rot(e, 21)^rot(e, 7))+(g^(e&(f^g)))+K+x
-        t2=(rot(a, 30)^rot(a, 19)^rot(a, 10))+((a&b)|(c&(a|b)))
-        return (uint32(d + t1), uint32(t1+t2))
+	t1=h+(rot(e, 26)^rot(e, 21)^rot(e, 7))+(g^(e&(f^g)))+K+x
+	t2=(rot(a, 30)^rot(a, 19)^rot(a, 10))+((a&b)|(c&(a|b)))
+	return (uint32(d + t1), uint32(t1+t2))
+
+def partial(state, data):
+	partial = np.array(state)
+	(partial[3], partial[7]) = sharound(partial[0],partial[1],partial[2],partial[3],partial[4],partial[5],partial[6],partial[7],data[0],0x428A2F98)
+	(partial[2], partial[6]) = sharound(partial[7],partial[0],partial[1],partial[2],partial[3],partial[4],partial[5],partial[6],data[1],0x71374491)
+	(partial[1], partial[5]) = sharound(partial[6],partial[7],partial[0],partial[1],partial[2],partial[3],partial[4],partial[5],data[2],0xB5C0FBCF)
+	return partial
 
 def if_else(condition, trueVal, falseVal):
-        if condition:
-                return trueVal
-        else:
-                return falseVal
+	if condition:
+		return trueVal
+	else:
+		return falseVal
+
+def bytereverse(x):
+	return ( ((x) << 24) | (((x) << 8) & 0x00ff0000) | (((x) >> 8) & 0x0000ff00) | ((x) >> 24) )
 
 class BitcoinMiner:
-        def __init__(self, platform, context, host, user, password, port=8332, frames=60, rate=10, askrate=5, worksize=-1, vectors=False):
-                (defines, self.maxBase, self.rateDivisor) = if_else(vectors, ('-DVECTORS', 0x7fffffff, 500), ('', 0xffffffff, 1000))
+	def __init__(self, platform, context, host, user, password, port=8332, frames=60, rate=1, askrate=5, worksize=-1, vectors=False):
+		(defines, self.rateDivisor) = if_else(vectors, ('-DVECTORS', 500), ('', 1000))
 
-                self.context = context
-                self.rate = int(rate)
-                self.askrate = int(askrate)
-                self.worksize = int(worksize)
-                
-                if (platform.name.lower().find('nvidia') != -1):
-                        defines += ' -DNVIDIA'
-                elif (self.context.devices[0].extensions.find('cl_amd_media_ops') != -1):
-                        defines += ' -DBITALIGN'
-                        
-                kernelFile = open('btc_miner.cl', 'r')
-                self.miner = cl.Program(self.context, kernelFile.read()).build(defines)
-                kernelFile.close()
+		self.context = context
+		self.rate = float(rate)
+		self.askrate = int(askrate)
+		self.worksize = int(worksize)
+		
+		if (platform.name.lower().find('nvidia') != -1):
+			defines += ' -DNVIDIA'
+		elif (self.context.devices[0].extensions.find('cl_amd_media_ops') != -1):
+			defines += ' -DBITALIGN'
+			
+		kernelFile = open('btc_miner.cl', 'r')
+		self.miner = cl.Program(self.context, kernelFile.read()).build(defines)
+		kernelFile.close()
 
-                if (self.worksize == -1):
-                        self.worksize = self.miner.search.get_work_group_info(cl.kernel_work_group_info.WORK_GROUP_SIZE, self.context.devices[0])
+		if (self.worksize == -1):
+			self.worksize = self.miner.search.get_work_group_info(cl.kernel_work_group_info.WORK_GROUP_SIZE, self.context.devices[0])
 
-                frame = float(1)/float(frames)
-                window = frame/30
-                self.upper = frame + window
-                self.lower = frame - window
+		frame = float(1)/float(frames)
+		window = frame/30
+		self.upper = frame + window
+		self.lower = frame - window
 
-                self.unit = self.worksize * 256
-                self.globalThreads = self.unit
+		self.unit = self.worksize * 256
+		self.globalThreads = self.unit
 
-                self.bitcoin = ServiceProxy('http://%s:%s@%s:%s' % (user, password, host, port))
+		self.bitcoin = ServiceProxy('http://%s:%s@%s:%s' % (user, password, host, port))
 
-        def say(self, format, args=()):
-                sys.stdout.write('\r                                        \r' + format % args)
-                sys.stdout.flush()
+	def say(self, format, args=()):
+		sys.stdout.write('\r                                        \r' + format % args)
+		sys.stdout.flush()
 
-        def sayLine(self, format, args=()):
-                sysWrite(format + '\n', args)
+	def sayLine(self, format, args=()):
+		self.say(format + '\n', args)
 
-        def blockFound(self, output):
-                # designed to be overridden
-                self.sayLine('found: %s, %s', (output, datetime.now().strftime("%d/%m/%Y %H:%M")))
-        
-        def mine(self):
-                work = {}
-                work['data'] = ''
-                output = np.zeros(2, np.uint32)
-
-                queue = cl.CommandQueue(self.context)
-                
-                threadsRun = 0
-                lastRate = time()
-
-                while True:
-                        try:
-                                work = self.bitcoin.getwork()
-                        except JSONRPCException, e:
-                                self.say('%s', e.error['message'])
-                                sleep(2)
-                                continue
-                        except IOError:
-                                self.say('Unable to communicate with bitcoin RPC')
-                                sleep(2)
-                                continue
-                        
-                        try:
-                                block2 = np.array(unpack('IIIIIIIIIIIIIIII', work['data'][128:].decode('hex')), dtype=np.uint32)
-                                state  = np.array(unpack('IIIIIIII',         work['midstate'].decode('hex')),       dtype=np.uint32)
-                                target = np.array(unpack('IIIIIIII',         work['target'].decode('hex')),      dtype=np.uint32)
-                        except:
-                                sayLine('Wrong data format from RPC!')
-                                sys.exit()
+	def blockFound(self, output):
+		# designed to be overridden
+		self.sayLine('found: %s, %s', (output, datetime.now().strftime("%d/%m/%Y %H:%M")))
 	
-                        state2 = np.array(state)
-                        (state2[3], state2[7]) = sharound(state2[0],state2[1],state2[2],state2[3],state2[4],state2[5],state2[6],state2[7],block2[0],0x428A2F98)
-                        (state2[2], state2[6]) = sharound(state2[7],state2[0],state2[1],state2[2],state2[3],state2[4],state2[5],state2[6],block2[1],0x71374491)
-                        (state2[1], state2[5]) = sharound(state2[6],state2[7],state2[0],state2[1],state2[2],state2[3],state2[4],state2[5],block2[2],0xB5C0FBCF)
+	def mine(self):
+		work = {}
+		work['data'] = ''
+		output = np.zeros(1, np.uint32)
+		output_buf = cl.Buffer(self.context, cl.mem_flags.WRITE_ONLY | cl.mem_flags.USE_HOST_PTR, hostbuf=output)
 
-                        output[0] = base = 0
-                        output_buf = cl.Buffer(self.context, cl.mem_flags.WRITE_ONLY | cl.mem_flags.USE_HOST_PTR, hostbuf=output)
+		queue = cl.CommandQueue(self.context)
 
-                        start = time()
-                        while True:
-                                if (output[0]):
-                                        work['data'] = work['data'][:152] + pack('I', long(output[0])).encode('hex') + work['data'][160:]
-                                        self.blockFound(output[0])
-                                        self.bitcoin.getwork(work['data'])
-                                        break
+		base = threadsRun = 0
+		lastRate = time()
+		while True:
+			try:
+				work = self.bitcoin.getwork()
+			except JSONRPCException, e:
+				self.say('%s', e.error['message'])
+				sleep(2)
+				continue
+			except IOError:
+				self.say('Unable to communicate with bitcoin RPC')
+				sleep(2)
+				continue
+			
+			try:
+				data   = np.array(unpack('IIIIIIIIIIIIIIII', work['data'][128:].decode('hex')), dtype=np.uint32)
+				state  = np.array(unpack('IIIIIIII',         work['midstate'].decode('hex')),   dtype=np.uint32)
+				target = np.array(unpack('IIIIIIII',         work['target'].decode('hex')),     dtype=np.uint32)
+			except:
+				self.sayLine('Wrong data format from RPC!')
+				sys.exit()
 
-                                if (time() - start > self.askrate or base + self.globalThreads == self.maxBase):
-                                        break
+			state2 = partial(state, data)
 
-                                base += self.globalThreads
-                                if (base + self.globalThreads > self.maxBase):
-                                        base = self.maxBase - self.globalThreads
+			start = lastNTime = time()
+			while True:
+				if (output[0]):
+					work['data'] = work['data'][:136] + pack('I', long(data[1])).encode('hex') + work['data'][144:152] + pack('I', long(output[0])).encode('hex') + work['data'][160:]
+					self.blockFound(output[0])
+					output[0] = 0
+					cl.enqueue_write_buffer(queue, output_buf, output)
+					self.bitcoin.getwork(work['data'])
+					break
 
-                                kernelStart = time()
-                                self.miner.search(	queue, (self.globalThreads, ), (self.worksize, ),
-						block2[0], block2[1], block2[2],
-						state[0], state[1], state[2], state[3], state[4], state[5], state[6], state[7],
-						state2[1], state2[2], state2[3], state2[5], state2[6], state2[7],
-						target[6], pack('I', base), output_buf)
-                                cl.enqueue_read_buffer(queue, output_buf, output).wait()
-                                kernelTime = time() - kernelStart
-                                threadsRun += self.globalThreads
+				if (time() - start > self.askrate):
+					break
 
-                                if (kernelTime < self.lower):
-                                        self.globalThreads += self.unit
-                                elif (kernelTime > self.upper and self.globalThreads != self.unit):
-                                        self.globalThreads -= self.unit
+				if (time() - lastNTime > 1):
+					data[1] = uint32(bytereverse(bytereverse(data[1]) + 1))
+					state2 = partial(state, data)
+					lastNTime = time()
 
-                                if (time() - lastRate > self.rate):
-                                        self.say('%s khash/s', int((threadsRun / (time() - lastRate)) / self.rateDivisor))
-                                        threadsRun = 0
-                                        lastRate = time()
+				base = uint32(base + self.globalThreads)
+				kernelStart = time()
+				self.miner.search(	queue, (self.globalThreads, ), (self.worksize, ),
+									data[0], data[1], data[2],
+									state[0], state[1], state[2], state[3], state[4], state[5], state[6], state[7],
+									state2[1], state2[2], state2[3], state2[5], state2[6], state2[7],
+									target[6], pack('I', base), output_buf)
+				cl.enqueue_read_buffer(queue, output_buf, output)
+				queue.finish()
+				kernelTime = time() - kernelStart
+				threadsRun += self.globalThreads
+
+				if (kernelTime < self.lower):
+					self.globalThreads += self.unit
+				elif (kernelTime > self.upper and self.globalThreads > self.unit):
+					self.globalThreads -= self.unit
+
+				if (time() - lastRate > self.rate):
+					self.say('%s khash/s', int((threadsRun / (time() - lastRate)) / self.rateDivisor))
+					threadsRun = 0
+					lastRate = time()
--- poclbm.py
+++ poclbm.py
@@ -1,45 +1,15 @@
 #!/usr/bin/python
 
-import sys
-import numpy as np
 import pyopencl as cl
-
-from struct import *
-from time import sleep, time
-from datetime import datetime
-from jsonrpc import ServiceProxy
 from optparse import OptionParser
-from jsonrpc.proxy import JSONRPCException
-
-
-def uint32(x):
-	return x & 0xffffffffL
-def rot(x, y):
-	return (x<<y | x>>(32-y))
-def sharound(a,b,c,d,e,f,g,h,x,K):
-	t1=h+(rot(e, 26)^rot(e, 21)^rot(e, 7))+(g^(e&(f^g)))+K+x
-	t2=(rot(a, 30)^rot(a, 19)^rot(a, 10))+((a&b)|(c&(a|b)))
-	return (uint32(d + t1), uint32(t1+t2))
-
-def sysWrite(format, args=()):
-	sys.stdout.write('\r                                        \r' + format % args)
-	sys.stdout.flush()
-
-def sysWriteLn(format, args=()):
-	sysWrite(format + '\n', args)
-
-def if_else(condition, trueVal, falseVal):
-	if condition:
-		return trueVal
-	else:
-		return falseVal
+from BitcoinMiner import *
 
 parser = OptionParser()
 parser.add_option('-u', '--user',     dest='user',     default='bitcoin',   help='user name')
-parser.add_option('--pass',           dest='password', default='password',  help='password')
-parser.add_option('-o', '--host',     dest='host',     default='127.0.0.1', help='RPC host')
-parser.add_option('-p', '--port',     dest='port',     default='8332',      help='RPC port')
-parser.add_option('-r', '--rate',     dest='rate',     default=1,           help='hash rate display interval in seconds, default=1', type='float')
+parser.add_option('--pass',	          dest='password', default='password',  help='password')
+parser.add_option('-o', '--host',	  dest='host',     default='127.0.0.1', help='RPC host')
+parser.add_option('-p', '--port',	  dest='port',     default='8332',      help='RPC port')
+parser.add_option('-r', '--rate',	  dest='rate',     default=1,           help='hash rate display interval in seconds, default=1', type='float')
 parser.add_option('-f', '--frames',   dest='frames',   default=60,          help='will try to bring single kernel execution to 1/frames seconds, default=60, increase this for less desktop lag', type='float')
 parser.add_option('-d', '--device',   dest='device',   default=-1,          help='use device by id, by default asks for device', type='int')
 parser.add_option('-a', '--askrate',  dest='askrate',  default=5,           help='how many seconds between getwork requests, default 5, max 30', type='int')
@@ -50,104 +20,15 @@ options.frames = max(options.frames, 1.1)
 options.askrate = max(options.askrate, 1)
 options.askrate = min(options.askrate, 30)
 
-(defines, maxBase, rateDivisor) = if_else(options.vectors, ('-DVECTORS', 0x7fffffff, 500), ('', 0xffffffff, 1000))
-
 platform = cl.get_platforms()[0]
-if (options.device != -1):
-	devices = platform.get_devices()
-	context = cl.Context([devices[options.device]], None, None)
-else:
-	print 'No device specified, you may use -d to specify ONLY ONE of the following\n'
-	context = cl.create_some_context()
-queue = cl.CommandQueue(context)
-if (platform.name.lower().find('nvidia') != -1):
-	defines += ' -DNVIDIA'
-elif (context.devices[0].extensions.find('cl_amd_media_ops') != -1):
-	defines += ' -DBITALIGN'
-kernelFile = open('btc_miner.cl', 'r')
-miner = cl.Program(context, kernelFile.read()).build(defines)
-kernelFile.close()
+devices = platform.get_devices()
 
-if (options.worksize == -1):
-	options.worksize = miner.search.get_work_group_info(cl.kernel_work_group_info.WORK_GROUP_SIZE, context.devices[0])
+if (options.device == -1 or options.device >= len(devices)):
+	print 'No device specified or device not found, use -d to specify one of the following\n'
+	for i in xrange(len(devices)):
+		print '[%d]\t%s' % (i, devices[i].name)
+	sys.exit()
 
-frames = options.frames
-frame = float(1)/frames
-window = frame/30
-upper = frame + window
-lower = frame - window
-
-unit = options.worksize * 256
-globalThreads = unit
-
-bitcoin = ServiceProxy('http://' + options.user + ':' + options.password + '@' + options.host + ':' + options.port)
-
-work = {}
-work['data'] = ''
-output = np.zeros(2, np.uint32)
-
-threadsRun = 0
-lastRate = time()
-
-while True:
-	try:
-		work = bitcoin.getwork()
-	except JSONRPCException, e:
-		sysWrite('%s', e.error['message'])
-		sleep(2)
-		continue
-	except IOError:
-		sysWrite('Unable to communicate with bitcoin RPC')
-		sleep(2)
-		continue
-
-	try:
-		block2 = np.array(unpack('IIIIIIIIIIIIIIII', work['data'][128:].decode('hex')), dtype=np.uint32)
-		state  = np.array(unpack('IIIIIIII',         work['midstate'].decode('hex')),       dtype=np.uint32)
-		target = np.array(unpack('IIIIIIII',         work['target'].decode('hex')),      dtype=np.uint32)
-	except:
-		sysWriteLn('Wrong data format from RPC!')
-		sys.exit()
-	
-	state2 = np.array(state)
-	(state2[3], state2[7]) = sharound(state2[0],state2[1],state2[2],state2[3],state2[4],state2[5],state2[6],state2[7],block2[0],0x428A2F98)
-	(state2[2], state2[6]) = sharound(state2[7],state2[0],state2[1],state2[2],state2[3],state2[4],state2[5],state2[6],block2[1],0x71374491)
-	(state2[1], state2[5]) = sharound(state2[6],state2[7],state2[0],state2[1],state2[2],state2[3],state2[4],state2[5],block2[2],0xB5C0FBCF)
-
-	output[0] = base = 0
-	output_buf = cl.Buffer(context, cl.mem_flags.WRITE_ONLY | cl.mem_flags.USE_HOST_PTR, hostbuf=output)
-
-	start = time()
-	while True:
-		if (output[0]):
-			work['data'] = work['data'][:152] + pack('I', long(output[0])).encode('hex') + work['data'][160:]
-			sysWriteLn('found: %s, %s', (output[0], datetime.now().strftime("%d/%m/%Y %H:%M")))
-			bitcoin.getwork(work['data'])
-			break
-
-		if (time() - start > options.askrate or base + globalThreads == maxBase):
-			break
-
-		base += globalThreads
-		if (base + globalThreads > maxBase):
-			base = maxBase - globalThreads
-
-		kernelStart = time()
-		miner.search(	queue, (globalThreads, ), (options.worksize, ),
-						block2[0], block2[1], block2[2],
-						state[0], state[1], state[2], state[3], state[4], state[5], state[6], state[7],
-						state2[1], state2[2], state2[3], state2[5], state2[6], state2[7],
-						target[6], pack('I', base), output_buf)
-		cl.enqueue_read_buffer(queue, output_buf, output).wait()
-		kernelTime = time() - kernelStart
-		threadsRun += globalThreads
-
-		if (kernelTime < lower):
-			globalThreads += unit
-		elif (kernelTime > upper and globalThreads != unit):
-			globalThreads -= unit
-
-		if (time() - lastRate > options.rate):
-			sysWrite('%s khash/s', int((threadsRun / (time() - lastRate)) / rateDivisor))
-			threadsRun = 0
-			lastRate = time()
+context = cl.Context([devices[options.device]], None, None)
+myMiner = BitcoinMiner(platform, context, options.host, options.user, options.password, options.port, options.frames, options.rate, options.askrate, options.worksize, options.vectors)
+myMiner.mine()
