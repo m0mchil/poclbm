99c57637ca0d1db70187ddcc8eb2d1abb5cc07cf
m0mchil <pishtov@gmail.com>
2010-12-07 15:56:41 +0200

    removed brocktice fork specific files

--- test_bitcoinminer.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-from BitcoinMiner import *
-platform = cl.get_platforms()[0]
-devices = platform.get_devices()
-context = cl.Context([devices[1]], None, None)
-myMiner = BitcoinMiner(platform, context, 'localhost', 'rpcusername_here', 'rpcpassword_here')
-myMiner.mine()
