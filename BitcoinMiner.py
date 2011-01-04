919b2c65af759784c3f6d3e65f6b5edc8e43a469
m0mchil <pishtov@gmail.com>
2011-01-04 16:25:52 +0200

    removed time update

--- BitcoinMiner.py
+++ BitcoinMiner.py
@@ -24,21 +24,11 @@ def sharound(a,b,c,d,e,f,g,h,x,K):
 	t2=(rot(a, 30)^rot(a, 19)^rot(a, 10))+((a&b)|(c&(a|b)))
 	return (uint32(d + t1), uint32(t1+t2))
 
-def partial(state, data):
-	partial = np.array(state)
-	(partial[3], partial[7]) = sharound(partial[0],partial[1],partial[2],partial[3],partial[4],partial[5],partial[6],partial[7],data[0],0x428A2F98)
-	(partial[2], partial[6]) = sharound(partial[7],partial[0],partial[1],partial[2],partial[3],partial[4],partial[5],partial[6],data[1],0x71374491)
-	(partial[1], partial[5]) = sharound(partial[6],partial[7],partial[0],partial[1],partial[2],partial[3],partial[4],partial[5],data[2],0xB5C0FBCF)
-	return partial
-
 def if_else(condition, trueVal, falseVal):
 	if condition:
 		return trueVal
 	else:
 		return falseVal
-
-def bytereverse(x):
-	return uint32(( ((x) << 24) | (((x) << 8) & 0x00ff0000) | (((x) >> 8) & 0x0000ff00) | ((x) >> 24) ))
 
 class BitcoinMiner(Thread):
 	def __init__(self, platform, context, host, user, password, port=8332, frames=60, rate=1, askrate=5, worksize=-1, vectors=False):
@@ -148,10 +138,13 @@ class BitcoinMiner(Thread):
 						data   = np.array(unpack('IIIIIIIIIIIIIIII', work['data'][128:].decode('hex')), dtype=np.uint32)
 						state  = np.array(unpack('IIIIIIII',         work['midstate'].decode('hex')),   dtype=np.uint32)
 						target = np.array(unpack('IIIIIIII',         work['target'].decode('hex')),     dtype=np.uint32)
-						state2 = partial(state, data)
 					except Exception as e:
 						self.sayLine('Wrong data format from RPC!')
 						sys.exit()
+					state2 = np.array(state)
+					(state2[3], state2[7]) = sharound(state2[0],state2[1],state2[2],state2[3],state2[4],state2[5],state2[6],state2[7],data[0],0x428A2F98)
+					(state2[2], state2[6]) = sharound(state2[7],state2[0],state2[1],state2[2],state2[3],state2[4],state2[5],state2[6],data[1],0x71374491)
+					(state2[1], state2[5]) = sharound(state2[6],state2[7],state2[0],state2[1],state2[2],state2[3],state2[4],state2[5],data[2],0xB5C0FBCF)
 
 			kernelStart = time()
 			self.miner.search(	queue, (globalThreads, ), (self.worksize, ),
@@ -180,14 +173,9 @@ class BitcoinMiner(Thread):
 			if output[0]:
 				result = {}
 				d = work['data']
-				d = d[:136] + pack('I', long(data[1])).encode('hex') + d[144:152] + pack('I', long(output[1])).encode('hex') + d[160:]
+				d = d[:152] + pack('I', long(output[1])).encode('hex') + d[160:]
 				result['data'] = d
 				result['hash'] = output[0]
 				self.resultQueue.put(result)
 				output[0] = 0
 				cl.enqueue_write_buffer(queue, output_buf, output)
-
-			if (time() - lastNTime > 1):
-				data[1] = bytereverse(bytereverse(data[1]) + 1)
-				state2 = partial(state, data)
-				lastNTime = time()
